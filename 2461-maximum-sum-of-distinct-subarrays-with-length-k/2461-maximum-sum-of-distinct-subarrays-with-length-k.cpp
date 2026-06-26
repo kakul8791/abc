b71acb498d1500970f9005a47994e85127cdcb1c
kakul rathi <rathikakul4@gmail.com>
2026-06-26 20:49:41 +0530

    Time: 99 ms (71.06%), Space: 95.2 MB (93.41%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
+        long long sum=0;
+        long long ans=0;
+        unordered_map<int,int>freq;
 
-        unordered_map<int,int> freq;
-        long long sum = 0;
-
-        for(int i = 0; i < k; i++) {
-            sum += nums[i];
+        for(int i=0;i<k;i++){
+            sum+=nums[i];
             freq[nums[i]]++;
         }
-
-        long long ans = 0;
-
-        if(freq.size() == k)
-            ans = sum;
-
-        for(int i = k; i < nums.size(); i++) {
-
-            sum += nums[i];
+        if(freq.size()==k){
+            ans=max(sum,ans);
+        }
+        for(int i=k;i<nums.size();i++){
+            sum+=nums[i];
             freq[nums[i]]++;
 
-            sum -= nums[i-k];
+            sum-=nums[i-k];
             freq[nums[i-k]]--;
 
-            if(freq[nums[i-k]] == 0)
+            if(freq[nums[i-k]]==0){
                 freq.erase(nums[i-k]);
+            }
 
-            if(freq.size() == k)
-                ans = max(ans, sum);
+            if(freq.size()==k){
+                ans=max(ans,sum);
+            }
+            
+            
         }
-
         return ans;
+        
     }
 };
